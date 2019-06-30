9a89717578115c82a787b23c49d83705c3923d0a
ngengesenior <seniorngenge@gmail.com>
2019-06-30 05:11:18 +0100

    Step1 complete

---
+++ app/src/main/java/com/cafermertceyhan/kotlineverywherecodelab/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.cafermertceyhan.kotlineverywherecodelab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
