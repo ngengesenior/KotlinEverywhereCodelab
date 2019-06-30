4c031d2f289596a085c01152f1843e9168391a29
ngengesenior <seniorngenge@gmail.com>
2019-06-30 05:49:43 +0100

    Step5 complete

--- app/src/main/java/com/cafermertceyhan/kotlineverywherecodelab/MainActivity.kt
+++ app/src/main/java/com/cafermertceyhan/kotlineverywherecodelab/MainActivity.kt
@@ -12,12 +12,16 @@ class MainActivity : AppCompatActivity() {
 
         val username = intent.getStringExtra("username")
         val email = intent.getStringExtra("email")
+        val password = intent.getStringExtra("password")
 
         //  TODO 8 - Get password value from intent
 
         tv_hello_user.text = getString(R.string.hello_user, username)
         tv_user_email.text = getString(R.string.your_email, email)
 
+
         // TODO 9 - Set R.string.your_password value to tv_user_password
+        tv_user_password.text = getString(R.string.your_password,password)
+
     }
 }
